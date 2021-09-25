b652b441c87035c088d1111547605aa00d531b05
jeantr35 <jean.trujillo.35@gmail.com>
2021-09-24 23:31:43 -0500

    Refactored first example, using method extraction

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -1,10 +1,10 @@
 
 public class TennisGame1 implements TennisGame {
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private int mScore1 = 0;
+    private int mScore2 = 0;
+    private final String player1Name;
+    private final String player2Name;
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -12,65 +12,62 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
+        if (playerName.equals(this.player1Name))
+            mScore1 += 1;
         else
-            m_score2 += 1;
+            mScore2 += 1;
     }
 
-    public String getScore() {
+    private String getEqualsScore(){
+        switch (mScore1)
+        {
+            case 0:
+                return  "Love-All";
+            case 1:
+                return "Fifteen-All";
+            case 2:
+                return  "Thirty-All";
+            default:
+                return  "Deuce";
+        }
+    }
+
+    private String getAdvantageScore(int minusResult){
+        if (minusResult==1) return "Advantage " + this.player1Name;
+        if (minusResult ==-1) return "Advantage " + this.player2Name;
+        if (minusResult>=2) return "Win for " + this.player1Name;
+        return "Win for " + this.player2Name;
+    }
+
+    private String getPlayerScore(int score){
+        switch(score)
+        {
+            case 0:
+                return "Love";
+            case 1:
+               return "Fifteen";
+            case 2:
+                return "Thirty";
+            default:
+                return "Forty";
+
+        }
+    }
+
+    private String setScores(int mScore1, int mScore2){
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
-        }
+        score = score.concat(getPlayerScore(mScore1));
+        score = score.concat("-");
+        score = score.concat(getPlayerScore(mScore2));
         return score;
+    }
+
+
+    public String getScore() {
+        if (mScore1 == mScore2)
+            return getEqualsScore();
+        if (mScore1 >=4 || mScore2 >=4)
+            return getAdvantageScore(mScore1 - mScore2);
+        return setScores(mScore1, mScore2);
     }
 }
